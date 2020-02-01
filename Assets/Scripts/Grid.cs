528c87bea428cc962821c9adb30f0f5cff95f29d
Paul Sztajer <paul@sztajer.org>
2020-02-01 12:09:44 +1100

    selecting a square

--- Assets/Scripts/Grid.cs
+++ Assets/Scripts/Grid.cs
@@ -4,5 +4,16 @@ using UnityEngine;
 
 public class Grid : MonoBehaviour
 {
-    
+    public GridSquare currentlySelected { get; private set; } = null;
+
+    public void SelectSquare(GridSquare square)
+    {
+        if (currentlySelected != null)
+        {
+            currentlySelected.Deselect();
+        }
+
+        currentlySelected = square;
+        square.Select();
+    }
 }
--- Assets/Scripts/GridSquare.cs
+++ Assets/Scripts/GridSquare.cs
@@ -4,4 +4,27 @@ using UnityEngine;
 
 public class GridSquare : MonoBehaviour
 {
+    public Grid grid {
+        get
+        {
+            if (_grid == null)
+            {
+                _grid = transform.parent.GetComponent<Grid>();
+            }
+            return _grid;
+        }
+    }
+    private Grid _grid;
+
+    public bool isSelected { get; private set; } = false;
+
+    public void Select()
+    {
+        isSelected = true;
+    }
+
+    public void Deselect()
+    {
+        isSelected = false;
+    }
 }
