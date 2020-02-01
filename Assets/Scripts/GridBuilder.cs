654e3d96acd3ff1d29356764091c9fd46a87b32b
Paul Sztajer <paul@sztajer.org>
2020-02-01 11:54:15 +1100

    Grid prefab and object

--- Assets/Scripts/GridBuilder.cs
+++ Assets/Scripts/GridBuilder.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+[RequireComponent(typeof(Grid))]
 public class GridBuilder : MonoBehaviour
 {
     public int width;
@@ -10,4 +11,17 @@ public class GridBuilder : MonoBehaviour
     public Vector2 size;
 
     public GridSquare GridSquarePrefab;
+
+    public Grid grid
+    {
+        get
+        {
+            if (_grid == null)
+            {
+                _grid = GetComponent<Grid>();
+            }
+            return _grid;
+        }
+    }
+    private Grid _grid;
 }
