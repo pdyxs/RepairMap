8d26687ead6ba1b9c190de32053c490c7ba666a5
CapelaGames <andrew@Capelagames.com.au>
2020-02-01 14:32:50 +1100

    animation and movement work

---
+++ Assets/Scripts/Character/Placement.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Placement : MonoBehaviour
+{
+
+    public int[,] DefaultGrid;
+
+    public Transform characterMovementTrans;
+    public Transform characterRotationTrans;
+
+    private void Start()
+    {
+        if(characterMovementTrans == null)
+        {
+            Debug.LogError("set characterMovementTrans in Placement");
+        }
+
+        if (characterRotationTrans == null)
+        {
+            Debug.LogError("set characterRotationTrans in Placement");
+        }
+    }
+
+
+
+}
